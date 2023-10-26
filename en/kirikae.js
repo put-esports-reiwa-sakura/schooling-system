050ae6153b3b99ca4871e06a811eceee4514e613
令和さくら高等学院プチe-sports部 <147113051+put-esports-reiwa-sakura@users.noreply.github.com>
2023-10-27 08:21:18 +0900

    Add files via upload

---
+++ en/kirikae.js
@@ -0,0 +1,67 @@
+// ©2023 REIWA SAKURA KOUTOU GAKUIN
+// ©2023 令和さくら高等学院
+// Access to https://www.reiwa-sakura.net/
+// がくいんちょう: のだ (teacher)
+// Main Programmer: Aoyama
+// Check: Akita (teacher),hutamura (teacher), hashimoto
+// Thank you to everyone who helped us.
+function getModeFromURL() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('mode');
+}
+function loadAppropriateStylesheet(mode) {
+  const link = document.getElementById('stylesheet-link');
+  if (mode === 'dark') {
+    link.href = 'color-black.css';
+  } else {
+    link.href = 'color-white.css';
+  }
+}
+function setupModeSwitch() {
+  const toggleSwitch = document.getElementById('mode-switch');
+  const savedState = loadSwitchState();
+  if (savedState === 'on') {
+    toggleSwitch.checked = true;
+    loadAppropriateStylesheet('dark');
+  } else {
+    toggleSwitch.checked = false;
+    loadAppropriateStylesheet('light');
+  }
+  toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+      saveSwitchState('on');
+      loadAppropriateStylesheet('dark');
+    } else {
+      saveSwitchState('off');
+      loadAppropriateStylesheet('light');
+    }
+  });
+}
+function saveSwitchState(state) {
+  localStorage.setItem('switchState', state);
+}
+function loadSwitchState() {
+  const savedState = localStorage.getItem('switchState');
+  return savedState !== null ? savedState : 'on';
+}
+window.addEventListener('DOMContentLoaded', () => {
+  const mode = getModeFromURL();
+  const toggleSwitch = document.getElementById('mode-switch');
+  
+  if (mode) {
+    loadAppropriateStylesheet(mode);
+    if (mode === 'dark') {
+      toggleSwitch.checked = true;
+    }
+  } else {
+    const savedState = loadSwitchState();
+    if (savedState === 'on') {
+      loadAppropriateStylesheet('dark');
+      toggleSwitch.checked = true;
+    } else {
+      loadAppropriateStylesheet('light');
+      toggleSwitch.checked = false;
+    }
+  }
+  setupModeSwitch();
+});
